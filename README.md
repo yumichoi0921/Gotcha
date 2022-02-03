1de68a393a282500c03521c23eeba7ccc927572d
안현호 <ahndding@gmail.com>
2022-02-03 10:36:42 +0900

    chore: 프로젝트 기본 셋팅

---
+++ README.md
@@ -0,0 +1,2 @@
+### YouBusted
+니선싸악깡버팀의 '딱걸렸쥬' repository입니다.
---
+++ back/Gotcha/src/main/java/com/saffy/gotcha/GotchaApplication.java
@@ -0,0 +1,13 @@
+package com.saffy.gotcha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GotchaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GotchaApplication.class, args);
+	}
+
+}
