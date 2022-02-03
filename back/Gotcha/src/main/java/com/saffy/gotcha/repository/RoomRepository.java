29a4a21e1e046461c42bebec91959c70791c2131
안현호 <ahndding@gmail.com>
2022-02-03 19:14:07 +0900

    feat: gameSessionRepository 구현 및 RoomRepository구현

---
+++ back/Gotcha/src/main/java/com/saffy/gotcha/repository/RoomRepository.java
@@ -0,0 +1,9 @@
+package com.saffy.gotcha.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.saffy.gotcha.entity.Room;
+
+public interface RoomRepository extends JpaRepository<Room, Long>{
+
+}
