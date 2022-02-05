8a0c8549ef286aeb82dcd74d986f44b2b4ece4fd
안현호 <ahndding@gmail.com>
2022-02-05 14:53:04 +0900

    rerere

---
+++ back/Gotcha/src/main/java/com/saffy/gotcha/config/WebConfiguration.java
@@ -0,0 +1,16 @@
+package com.saffy.gotcha.config;
+
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+public class WebConfiguration implements WebMvcConfigurer {
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/**").allowedOrigins("*").allowedMethods("*")
+				.allowedHeaders("Content-Type", "X-AUTH-TOKEN", "Authorization", "Access-Control-Allow-Origin",
+						"Access-Control-Allow-Credentials")
+				.exposedHeaders("Content-Disposition", "X-AUTH-TOKEN", "Authorization", "Access-Control-Allow-Origin",
+						"Access-Control-Allow-Credentials")
+				.allowCredentials(true).maxAge(3600);
+	}
+}
