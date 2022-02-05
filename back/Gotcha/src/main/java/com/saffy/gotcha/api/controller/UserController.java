b213d5c80e38eb7deb910eba0607cadbb560bbcd
안현호 <ahndding@gmail.com>
2022-02-05 14:57:45 +0900

    fix: webconfiguration으로 인한 controller별 crossorigin어노테이션 제거

--- back/Gotcha/src/main/java/com/saffy/gotcha/api/controller/UserController.java
+++ back/Gotcha/src/main/java/com/saffy/gotcha/api/controller/UserController.java
@@ -1,10 +1,7 @@
 package com.saffy.gotcha.api.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,7 +15,6 @@ import com.saffy.gotcha.entity.User;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 
-@CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
 @RequestMapping("/api/users")
 @Api(value = "User 컨트롤러 API")
