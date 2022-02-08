6b75698bd7b5b7e61b594a4b137cbe1ec3e7da0e
yumichoi <yumichoi0921@gmail.com>
2022-02-09 00:43:44 +0900

    fix: 뷰 그리드 시스템으로 변경

--- front/src/views/JokerGame.vue
+++ front/src/views/JokerGame.vue
@@ -1,55 +1,55 @@
 <template>
   <div id="JockerGame">
-    <b-row cols="5" id="SubscriberSection" class="mb-3">
+    <div id="SubscriberSection" class="row row-cols-5 mb-3">
       <b-col
         v-for="sub in subscribers"
         :key="sub.stream.connection.connectionId"
       >
         <user-video :stream-manager="sub"
       /></b-col>
-    </b-row>
-    <b-row id="PublisherSection">
-      <b-col cols="8" id="CardSection">
-        <!-- <b-alert show variant="secondary"
-          >여기는 잼민이의 메시지가 나오는 곳입니다아</b-alert
-        > -->
-        <b-row id="JamminMessage" class="h-25">
+    </div>
+    <div id="PublisherSection" class="row">
+      <div id="GameInfoSection" class="col-8">
+        <div id="JamminMessage" class="row h-25">
           <b-col>
             <b-alert show variant="secondary"
               >여기는 잼민이의 메시지가 나오는 곳입니다아</b-alert
             ></b-col
           >
-        </b-row>
-        <b-row>
-          <b-col>
+        </div>
+        <div id="CardInfoSection" class="row">
+          <div id="SubCardDeck" class="col">
             <b-alert show variant="primary">카드를 선택하세요</b-alert>
-            <b-col v-for="(card, idx) in cards[myturn]" v-bind:key="idx">
-              <b-card
-                v-on:click="cardClick(card)"
-                :img-src="require('../assets/poker/backCard.jpg')"
-                class="cardlist my-3"
-              >
-                <!-- {{ card.shape }} {{ card.number }} -->
-              </b-card>
-            </b-col>
-          </b-col>
-          <!--  </b-card-group> -->
-          <b-col>
+            <b-row cols="6" class="">
+              <b-col v-for="(card, idx) in cards[myturn]" v-bind:key="idx">
+                <b-card
+                  v-on:click="cardClick(card)"
+                  :img-src="require('../assets/poker/backCard.jpg')"
+                  class="cardlist my-3"
+                >
+                  <!-- {{ card.shape }} {{ card.number }} -->
+                </b-card>
+              </b-col></b-row
+            >
+          </div>
+          <div id="PubCardDeck" class="col">
             <b-alert show variant="primary">내카드덱</b-alert>
             <!-- <b-button variant="primary" v-on:click="makeDeck()"
                 >Primary</b-button
               > -->
-            <b-col v-for="(card, idx) in cards[myturn]" v-bind:key="idx">
-              <img
-                class="cardlist"
-                v-on:click="cardClick(card)"
-                :src="require('@/assets/poker/poker' + card + '.jpg')"
-              />
-            </b-col>
-          </b-col>
-        </b-row>
-      </b-col>
-      <b-col cols="4" id="PublisherUserInfo">
+            <b-row cols="6" class="">
+              <b-col v-for="(card, idx) in cards[myturn]" v-bind:key="idx">
+                <img
+                  class="cardlist"
+                  v-on:click="cardClick(card)"
+                  :src="require('@/assets/poker/poker' + card + '.jpg')"
+                /> </b-col
+            ></b-row>
+          </div>
+        </div>
+      </div>
+
+      <div id="PublisherInfo" class="col">
         <div>
           <user-video
             :stream-manager="publisher"
@@ -57,8 +57,8 @@
             :myCard="myCard"
           />
         </div>
-      </b-col>
-    </b-row>
+      </div>
+    </div>
   </div>
 </template>
 
