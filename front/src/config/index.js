36a0a1b2fb4cbe94068226c0800a15a729922206
안현호 <ahndding@gmail.com>
2022-02-12 00:43:51 +0900

    feat: dockerize 대비 주석 추가

--- front/src/config/index.js
+++ front/src/config/index.js
@@ -1,2 +1,3 @@
-const API_BASE_URL = "http://localhost:8080";
+// const API_BASE_URL = "https://i6b102.p.ssafy.io"; // 배포용
+const API_BASE_URL = "http://localhost:8080"; // 테스트용 (local)
 export { API_BASE_URL };
